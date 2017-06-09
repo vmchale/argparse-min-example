cf5d9973be01afa40efd909b1640ab539d5231d7
Vanessa McHale <tmchale@wisc.edu>
2017-06-09 00:40:24 -0500

    first working commit

---
+++ argparse_min_example/__init__.py
@@ -0,0 +1,7 @@
+import argparse
+
+def parser():
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--language1', dest='lang', metavar='བོ་སྐད་གི་', type=str, help='Lanugage for output')
+    parser.add_argument('--language2', dest='lang', metavar='LANGUAGE', type=str, help='Lanugage for output')
+    return parser.parse_args()
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='argparse_min_example',
+      version='0.1',
+      description='',
+      url='http://github.com/vmchale/argparse-min-example',
+      author='Vanessa McHale',
+      author_email='vamchale@gmail.com',
+      license='BSD3',
+      packages=['argparse_min_example'],
+      scripts=['bin/argparse-min-example'],
+      install_requires=[],
+      zip_safe=False)
